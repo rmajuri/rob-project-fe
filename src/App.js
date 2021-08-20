78bae60170e9dfdc4aa287ee4f69bada8adb89f1
Rob Majuri <rmajuri@urbn.com>
2021-08-20 13:51:40 -0400

    Add GET and DELETE functionality

--- src/App.js
+++ src/App.js
@@ -3,20 +3,41 @@ import './App.css';
 import Article from './article'
 
 import React, { useState, useEffect } from 'react';
+import Typography from '@material-ui/core/Typography';
 
 const App = () => {
   const [articles, setArticles] = useState([])
 
 
   const fetchArticles = () => {
-    fetch('http://127.0.0.1:8000/articles/', {method: 'GET', headers: {'Content-Type': 'application/json'}})
+    fetch('http://127.0.0.1:8000/articles/', { method: 'GET', headers: { 'Content-Type': 'application/json' } })
       .then((res) => {
         return res.json()
       })
       .then((articleResults) => {
         console.log(articleResults)
         setArticles(articleResults)
-        
+
+      })
+      .catch((err) => {
+        console.error(err)
+      })
+  }
+
+  const handleDeleteClick = (articleId) => {
+    const url = `http://127.0.0.1:8000/articles/${articleId}/`
+
+    fetch(url, { method: 'DELETE', headers: { 'Content-Type': 'application/json' } })
+      .then((res) => {
+        return res.status
+      })
+      .then((status) => {
+        console.log(status)
+        if (status === 204) {
+          setArticles(articles.filter(article => article.id !== articleId))
+        } else {
+          throw 'Failure while trying to delete article'
+        }
       })
       .catch((err) => {
         console.error(err)
@@ -28,12 +49,18 @@ const App = () => {
     fetchArticles()
   }, [])
 
-  const renderArticles = articles.map(article => <Article {...article}/>) 
+  const renderArticles = articles.map(article => <Article key={article.id} {...article} deleteClickHandler={handleDeleteClick} />)
 
 
   return (
     <div className="App">
-    {articles ? renderArticles : null}
+      <Typography gutterBottom variant="h4" component="header">
+        News Tracker
+      </Typography>
+      <Typography gutterBottom variant="subtitle1" component="header">
+        Curate a collection of news articles from around the web.
+      </Typography>
+      {articles ? renderArticles : null}
     </div>
   )
 }
--- src/article.js
+++ src/article.js
@@ -1,12 +1,55 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
-const Article = ({ name, link, description }) => {
+const useStyles = makeStyles({
+    root: {
+      maxWidth: 345,
+      marginBottom: "1rem"
+    },
+  });
 
-    return (<div>
-        <p>{name}</p>
-        <p>{description}</p>
-        <a href={link}>Go to Article</a>
-    </div>)
+const Article = ({ id, name, link, description, deleteClickHandler }) => {
+
+    const classes = useStyles();
+
+    
+
+  return (
+    <Card className={classes.root}>
+      <CardActionArea>
+        {/* <CardMedia
+          component="img"
+          alt="Contemplative Reptile"
+          height="140"
+          image="/static/images/cards/contemplative-reptile.jpg"
+          title="Contemplative Reptile"
+        /> */}
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {name}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+           {description}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <a href={link} target="_blank"><Button size="small" color="primary">
+          Go to Article
+        </Button></a>
+        <Button size="small" color="primary" onClick={() => deleteClickHandler(id)}>
+          Delete
+        </Button>
+      </CardActions>
+    </Card>
+  );
 
 }
 
